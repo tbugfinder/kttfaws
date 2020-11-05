26150213a62729a7267e5e623a07ce9c40455b8d
tbugfinder <github@online.ms>
2020-11-05 08:20:10 +0100

    use proper variables and reformat

--- test/integration/default/controls/default.rb
+++ test/integration/default/controls/default.rb
@@ -4,29 +4,37 @@ pp "***************************"
 pp "pp fixture_output1"
 pp fixture_output1
 
-role_id = JSON.generate(fixture_output1.first)
-role_id_h = JSON.parse(role_id, {:symbolize_names => false})
+tfoutput_json_helper = JSON.generate(fixture_output1.first)
+tfoutput_json = JSON.parse(tfoutput_json_helper, {:symbolize_names => false})
 pp "#*#*#*#*#*#*#"
-new_id=role_id_h['role_definition'].first['id']
-pp new_id
-new_id_clean = new_id.gsub("|",'')
+role_id=tfoutput_json['role_definition'].first['role_definition_resource_id']
+pp role_id
+pp "#*#*#*#*#*#*#"
+role_name = tfoutput_json['role_definition'].first['name']
+pp role_name
 pp "#*#*#*#*#*#*#"
 
-control 'generictest' do
-#  describe azure_generic_resource(resource_id: new_id) do # resource_id only supports resourceGroup objects
-#  describe azure_generic_resource(name: role_id_h['role_definition'].first['name'], resource_provider: 'Microsoft.Authorization/roleDefinitions') do # empty
-#  describe azure_generic_resource(name: role_id_h['role_definition'].first['name'], resource_uri: '/providers/Microsoft.Authorization/roleDefinitions/'+role_id_h['role_definition'].first['role_definition_id']), add_subscription_id: true) do # implicit conversion from symbol to integer
-  describe azure_generic_resource(name: role_id_h['role_definition'].first['name'], resource_uri: new_id_clean, add_subscription_id: false) do # implicit conversion from symbol to integer
+control 'generictest_id' do
+  describe azure_generic_resource(resource_id: role_id) do
     it { should exist }
-    #its('property') { should eq 'value' }
+    its('properties.permissions')         { should include 'Actions' }
+    its('properties.permissions.actions') { should include 'WhichAreTheProperties' } #  undefined method `actions' for #<Array:0x00007f2ea11f68d8>
+  end
+end
+
+control 'generictest_name' do
+  describe azure_generic_resource(name: role_name) do
+    it { should exist }
   end
 end
 
 control 'resourcetest' do
-  describe azurerm_role_definition(name: role_id_h['role_definition'].first['name']) do
+  #describe azure_role_definition(name: role_name) do
+  describe azure_role_definition(resource_id: role_id) do
     it { should exist }
-#    its('permissions_allowed') { should include 'Microsoft.Authorization/policyassignments/read'}
-#    its('permissions_allowed') { should_not include 'Microsoft.Authorization/policyassignments/write'}
-#    its('permissions_allowed') { should_not include '*'}
+    its('permissions') { should include 'less'}
+    its('permissions_allowed') { should include 'Microsoft.Authorization/policyassignments/read'}
+    its('permissions_allowed') { should_not include 'Microsoft.Authorization/policyassignments/write'}
+    its('permissions_allowed') { should_not include '*'}
   end
 end
